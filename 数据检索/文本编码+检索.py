d2abf546d8c01e7c0fcfa8f91c9638da6a57f24e
lihaodong <2409261729@qq.com>
2026-01-24 20:12:01 +0800

    文本编码+检索

--- 数据检索/文本编码+检索.py
+++ 数据检索/文本编码+检索.py
@@ -44,9 +44,10 @@ def chroma_retriever_query(text , k ):
         print(f"[{idx + 1}] 内容：{doc.page_content[:200]}...")  # 只显示前200个字符
 
 if __name__ == "__main__":
+    # 加载嵌入模型
     embedding_model = HuggingFaceEmbeddings(
-        model_name="../model/bge-base-zh-v1.5",
-        model_kwargs={"device": "cuda" if torch.cuda.is_available() else "cpu"},
+        model_name="D:/github/RAG_self/model/bge-base-zh-v1.5",
+        model_kwargs={"device": "cpu"},
         encode_kwargs={
             "normalize_embeddings": True
         },  # 输出归一化向量，更适合余弦相似度计算
@@ -54,10 +55,11 @@ if __name__ == "__main__":
     # 加载已有向量库
     vectorstore = Chroma(
         embedding_function=embedding_model,
-        persist_directory="D:/github/RAG_self/数据处理/vectorstore_rag",
+        persist_directory="../vectorstore_rag",
     )
 
     query_text = "宣告该自然人死亡应该是怎么样子才行"
-    print(vectorstore.similarity_search(query_text, k=3))
+    # print(vectorstore.similarity_search(query_text, k=3))
     # chroma_max_marginal_query(query_text, k=5)
-    # chroma_retriever_query(query_text, k=5)
+    chroma_retriever_query(query_text, k=5)
+
