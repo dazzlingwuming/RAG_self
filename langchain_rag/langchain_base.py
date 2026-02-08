89d61adaeeb3cc29e7a78268fde46ad23d16adf9
lihaodong <2409261729@qq.com>
2026-02-08 21:15:11 +0800

    基础组件

--- langchain_rag/langchain_base.py
+++ langchain_rag/langchain_base.py
@@ -6,13 +6,13 @@ from langchain_core.prompts import PromptTemplate
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_openai import ChatOpenAI
 import config.configRag as config
-from untils.untils_rag import format_docs
+from untils.untils_rag import format_docs_queries
 
 
 #调用大模型api
 def get_llm():
     # 大模型
-    load_dotenv("../../config/.env")  # 加载环境变量文件 .env
+    load_dotenv(config.dotenv_path)  # 加载环境变量文件 .env
     api_key = os.getenv("API_KEY")  # 从环境
     base_url = os.getenv("BASE_URL")  # 从环境变量获取 API 基础 URL
     model_name = os.getenv("MODEL_NAME")  # 从环境变量获取模型名称
@@ -67,7 +67,7 @@ def get_rerank_model(model_path):
     return model, tokenizer
 
 #基础问答
-def llm_chain(llm):
+def llm_chain_retrieve(llm):
     """
     基础问答链
     :param inputs: 包含 "context"（背景资料）-->list[Document]、"history"（历史消息）和 "query"（问题）的输入字典
@@ -92,7 +92,7 @@ def llm_chain(llm):
     # 定义 RAG 链条
     rag_chain = (
             {
-                "context": lambda x: format_docs(x.get("context")),
+                "context": lambda x: format_docs_queries(x.get("context")),
                 "history": lambda x: x.get("history"),
                 "query": lambda x: x.get("query"),
             }
@@ -104,6 +104,41 @@ def llm_chain(llm):
 
     return rag_chain
 
+
+# 基础问答
+def llm_chain_unretrieve(llm):
+    """
+    基础问答链
+    :param inputs: 包含 "context"（背景资料）-->list[Document]、"history"（历史消息）和 "query"（问题）的输入字典
+    :param llm: 大模型实例
+    :return: 大模型的回答字符串
+    """
+    # 提示词
+    prompt = PromptTemplate.from_template(
+        '''你是一个专业的中文问答助手，擅长基于提供的资料回答问题。
+请仅根据历史消息和你本身的知识回答问题，如无法找到答案，请直接回答“我不知道”。
+
+历史消息：[{history}]
+
+问题：{query}
+
+回答：''',
+
+    )
+
+    # 定义 RAG 链条
+    rag_chain = (
+            {
+                "history": lambda x: x.get("history"),
+                "query": lambda x: x.get("query"),
+            }
+            | prompt
+            | (lambda x: print(x.text, end="") or x)
+            | llm
+            | StrOutputParser()  # 输出解析器，将输出解析为字符串
+    )
+
+    return rag_chain
 
 
 if __name__ == '__main__':
